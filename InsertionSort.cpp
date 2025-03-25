a287736ce50b8c6d887b3c98db7928f8cd806cfa
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-03-25 13:48:13 +0700

    "Menambahkan fungsi input dengan validasi"

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,3 +3,32 @@
 
    int arr[20]; // Membuat Array dengan panjang data 20
    int n;       // Membuat Variable inputan n
+
+   void input()
+   { // procedure Input
+       while (true)
+       {
+           cout << "Masukkan Jumlah Data pada Array (maksimal 20): ";
+           cin >> n;
+
+           if (n <= 20)
+           { // Membuat Kondisi n tidak lebih dari 20
+               break;
+           }
+           else
+           {
+             cout << "/nArray yang anda masukkan maksimal 20 elemen./n"; // Menampilkan pesan jika data lebih dari 20
+           }
+       }
+
+       cout << endl;
+       cout << "=====================" << endl;
+       cout << "Masukkan Element Array" << endl;
+       cout << "=====================" <<endl;
+
+       for (int i = 0; i < n; i++)
+       {
+         cout << "Data ke-" << (i + 1) << ": ";
+         cin >> arr[i];
+       }
+   }
