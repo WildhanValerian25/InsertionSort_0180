af0d59c8308fcbf44db66e5f8fbbb53bedfe70ee
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-03-25 13:57:53 +0700

    "Mengimplementasikan algoritma insertion sort"

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -32,3 +32,24 @@
          cin >> arr[i];
        }
    }
+
+   void insertionSort()
+   { // procedure InsertionSort 
+      int temp, j; // Membuat variable data temporer dan index j
+
+      for (int i = 1; i < n; i++)
+      { // Step 1
+          temp = arr[i]; // Step 2
+          j = i - 1;     // Step 3
+
+          while (j >= 0 && arr [j] >temp)
+          {                      // Step 4
+            arr[j + 1] = arr[j]; // Step 4a
+            j--;                 // Step 4b
+          }
+         
+        arr[j + 1] = temp; // Step 5
+        }
+   }
+
+   void display()
